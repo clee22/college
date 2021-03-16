82f77e48f1657fcd16965ae84c561293ed520edc
Christopher N. Lee <43392988+clee22@users.noreply.github.com>
2021-03-16 16:28:00 -0400

    Update AR_Cube_Tracker_Follow.py

--- Comp 4500/Lab 3/AR_Cube_Tracker_Follow.py
+++ Comp 4500/Lab 3/AR_Cube_Tracker_Follow.py
@@ -10,6 +10,7 @@ import os
 from glob import glob
 
 from statemachine import *
+from cozmo.objects import CustomObject, CustomObjectMarkers, CustomObjectTypes
 
 try:
     from PIL import ImageDraw, ImageFont
@@ -17,6 +18,41 @@ except ImportError:
     sys.exit('run `pip3 install --user Pillow numpy` to run this example')
 def nothing(x):
     pass
+
+### stuff for custom object handling below
+ 
+def handle_object_appeared(evt, **kw):
+    # This will be called whenever an EvtObjectAppeared is dispatched -
+    # whenever an Object comes into view.
+    if isinstance(evt.obj, CustomObject):
+        print("Cozmo started seeing a %s" % str(evt.obj.object_type))
+
+
+def handle_object_disappeared(evt, **kw):
+    # This will be called whenever an EvtObjectDisappeared is dispatched -
+    # whenever an Object goes out of view.
+    if isinstance(evt.obj, CustomObject):
+        print("Cozmo stopped seeing a %s" % str(evt.obj.object_type))
+
+def custom_objects(robot: cozmo.robot.Robot):
+    # Add event handlers for whenever Cozmo sees a new object
+    robot.add_event_handler(cozmo.objects.EvtObjectAppeared, handle_object_appeared)
+    robot.add_event_handler(cozmo.objects.EvtObjectDisappeared, handle_object_disappeared)
+
+    # define the target cube which has the circles4 AR symbol
+    target_obj = robot.world.define_custom_cube(CustomObjectTypes.CustomType00,
+                                              CustomObjectMarkers.Circles4,
+                                              44,
+                                              30, 30, True)
+    # define the non importan/tracked cube as a distraction and has the diamonds2 AR symbol
+    distractor_obj = robot.world.define_custom_cube(CustomObjectTypes.CustomType01,
+                                              CustomObjectMarkers.Diamonds2,
+                                              44,
+                                              30, 30, True)
+
+### end of stuff for handling custom objects
+
+### stuff for handling states below
 
 def start_transitions(txt):
     cube_pos = [None, None, None, None, None] # 5 length array intended for cube positions to tell if cube is going 
@@ -33,15 +69,18 @@ def left_search_state_transitions(txt):
 
 async def run(robot: cozmo.robot.Robot):
 
-    m = StateMachine()
-    m.add_state("Start", start_transitions)
-    m.add_state("left_search", left_search_state_transitions)
-    m.add_state("right_search", right_search_state_transitions)
-    m.add_state("track", track_state_transitions)
-    m.add_state("approach", approach_state_transitions)
-    m.add_state("exit", None, end_state=1)
-    m.set_start("Start")
-    m.run()
+    # below is a singular line i added to make sure cozmo's head always starts out looking straight and not down or up
+    robot.set_head_angle(cozmo.util.degrees(-10), 0.0, 2.0, 0.5, True, False, 10)
+
+    fsm = StateMachine()
+    fsm.add_state("Start", start_transitions)
+    fsm.add_state("left_search", left_search_state_transitions)
+    fsm.add_state("right_search", right_search_state_transitions)
+    fsm.add_state("track", track_state_transitions)
+    fsm.add_state("approach", approach_state_transitions)
+    fsm.add_state("exit", None, end_state=1)
+    fsm.set_start("Start")
+    fsm.run()
 
 
 if __name__ == '__main__':
